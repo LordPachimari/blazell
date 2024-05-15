af3b0850c47abce66f3d8b1672e337c83dbb1c87
LordPachimari <opachimari@gmail.com>
2024-05-16 03:31:34 +1000

    fix lint

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,7 +1,5 @@
-import { View } from "react-native";
-
 function PostCard(props: {
 	onDelete: () => void;
 }) {
-	return <View className="flex flex-row rounded-lg bg-muted p-4"></View>;
+	return <></>;
 }
