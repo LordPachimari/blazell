7ecba0a3e2d03d650cd8e179c2d4f7f6429bc43b
LordPachimari <opachimari@gmail.com>
2024-06-12 19:50:49 +1000

    change env

--- apps/web/load-context.ts
+++ apps/web/load-context.ts
@@ -1,14 +1,6 @@
 import type { PlatformProxy } from "wrangler";
 import { z } from "zod";
 
-// When using `wrangler.toml` to configure bindings,
-// `wrangler types` will generate types for those bindings
-// into the global `Env` interface.
-// Need this empty interface so that typechecking passes
-// even if no `wrangler.toml` exists.
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-// biome-ignore lint/suspicious/noEmptyInterface: <explanation>
-
 type Cloudflare = Omit<PlatformProxy<AppEnv>, "dispose">;
 export const AppEnvSchema = z.object({
 	REPLICACHE_KEY: z.string(),
