a940b9569c2161ff3b50f2ca1839422354d99dfc
LordPachimari <opachimari@gmail.com>
2024-07-23 04:56:05 +0700

    change layout

--- apps/web/app/routes/marketplace+/components/products.tsx
+++ apps/web/app/routes/marketplace+/components/products.tsx
@@ -47,7 +47,7 @@ const ProductCard = ({ product }: { product: PublishedProduct }) => {
 			to={`/marketplace/products/${product.defaultVariant.handle}`}
 			prefetch="intent"
 			className={cn(
-				"group border min-h-20 min-w-20 hover:scale-[103%] col-span-1 row-span-1 hover:z-20 cursor-pointer border-border h-fit rounded-lg transition-all duration-200 ease-in-out",
+				"group border min-h-20 min-w-20 hover:scale-[103%] overflow-hidden col-span-1 row-span-1 hover:z-20 cursor-pointer border-border h-fit rounded-lg transition-all duration-200 ease-in-out",
 				{
 					"col-span-2 row-span-2": (product.score ?? 0) > 1,
 				},
@@ -55,41 +55,42 @@ const ProductCard = ({ product }: { product: PublishedProduct }) => {
 			unstable_viewTransition={true}
 			preventScrollReset={true}
 		>
-			{product.defaultVariant.thumbnail ? (
-				<Image
-					className="h-auto aspect-square max-w-full rounded-lg hover:brightness-50"
-					src={product.defaultVariant?.thumbnail?.url}
-					alt=""
-					fit="contain"
-					quality={100}
-				/>
-			) : (
-				<div className="aspect-square min-h-20 bg-component max-w-full rounded-lg hover:brightness-50">
-					<ImagePlaceholder />
-				</div>
-			)}
-			<div className="absolute inset-0 bg-gradient-to-b from-transparent via-black-a-7 dark:from-transparent dark:to-black-a-6 to-black-a-11 rounded-lg opacity-0 group-hover:opacity-70 transition duration-450 ease-in-out" />
-			<div className="flex absolute flex-end w-full px-2 bottom-0  rounded-b-lg">
-				<div className="group-hover:opacity-100 rounded-b-lg w-full opacity-0 transition-all duration-200 ease-in-out p-2">
-					<h1 className="font-freeman line-clamp-1 group-hover:blur-1px text-white dark:text-black text-ellipsis overflow-hidden">
-						{product.defaultVariant.title}
-					</h1>
-					<div className="hidden lg:block ">
-						<p className="overflow-hidden line-clamp-2 text-xs text-slate-2 group-hover:blur-1px font-semibold">
-							{product.defaultVariant.description ?? ""}
-						</p>
+			<div className="relative w-full h-full">
+				{product.defaultVariant.thumbnail ? (
+					<Image
+						className="h-auto aspect-square max-w-full rounded-lg hover:brightness-50"
+						src={product.defaultVariant?.thumbnail?.url}
+						alt=""
+						fit="contain"
+						quality={100}
+					/>
+				) : (
+					<div className="aspect-square min-h-20 bg-component max-w-full rounded-lg hover:brightness-50">
+						<ImagePlaceholder />
+					</div>
+				)}
+				<div className="absolute inset-0 bg-gradient-to-b from-transparent via-black-a-7 dark:from-transparent dark:to-black-a-6 to-black-a-11 rounded-lg opacity-0 group-hover:opacity-70 transition duration-450 ease-in-out" />
+				<div className="absolute inset-x-0 bottom-0 px-2 rounded-b-lg">
+					<div className="group-hover:opacity-100 rounded-b-lg w-full opacity-0 transition-all duration-200 ease-in-out p-2">
+						<h1 className="font-freeman line-clamp-1 group-hover:blur-1px text-white dark:text-black text-ellipsis overflow-hidden">
+							{product.defaultVariant.title}
+						</h1>
+						<div className="hidden lg:block ">
+							<p className="overflow-hidden line-clamp-2 text-xs text-slate-2 group-hover:blur-1px font-semibold">
+								{product.defaultVariant.description ?? ""}
+							</p>
+						</div>
 					</div>
 				</div>
+				<span className="absolute top-2 right-2 text-brand-9 font-freeman flex gap-2 text-sm md:text-base border border-brand-9 backdrop-blur-md rounded-lg p-1">
+					<Price
+						className="text-xs md:text-sm font-freeman flex-none text-brand-9 rounded-lg"
+						amount={product.defaultVariant.prices[0]!.amount}
+						currencyCode={product.defaultVariant.prices[0]!.currencyCode}
+						currencyCodeClassName="hidden @[275px]/label:inline"
+					/>
+				</span>
 			</div>
-
-			<span className="absolute top-2 right-2 text-brand-9 font-freeman flex gap-2 text-sm md:text-base border border-brand-9 backdrop-blur-md rounded-lg p-1">
-				<Price
-					className="text-xs md:text-sm font-freeman flex-none text-brand-9 rounded-lg"
-					amount={product.defaultVariant.prices[0]!.amount}
-					currencyCode={product.defaultVariant.prices[0]!.currencyCode}
-					currencyCodeClassName="hidden @[275px]/label:inline"
-				/>
-			</span>
 		</Link>
 	);
 };
