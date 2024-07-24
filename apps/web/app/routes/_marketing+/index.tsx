1ddb31aca6ae4337afdd4f1b38a0b987d5781849
LordPachimari <opachimari@gmail.com>
2024-07-24 10:10:31 +0700

    change message

--- apps/web/app/routes/_marketing+/index.tsx
+++ apps/web/app/routes/_marketing+/index.tsx
@@ -23,7 +23,7 @@ export default function Index() {
 				<Badge className="bg-brand-5 h-6 border-brand-6 text-brand-9">
 					Beta
 				</Badge>
-				Under develpoment. Many features and designs are not yet implemented.
+				Under development.
 				<Icons.Right className="size-4" />
 			</div>
 			<Hero />
