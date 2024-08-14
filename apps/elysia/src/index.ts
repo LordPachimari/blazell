d50ea4e801b44a2e7245b7329c5e5a377d625573
LordPachimari <opachimari@gmail.com>
2024-08-14 23:46:15 +0700

    change s3 to resend

--- apps/elysia/src/index.ts
+++ apps/elysia/src/index.ts
@@ -3,6 +3,7 @@ import { cors } from "@elysiajs/cors";
 import { Elysia, error } from "elysia";
 import { z } from "zod";
 import { getOtpHTML } from "./email/verification";
+import { Resend } from "resend";
 
 const VerifyOTPSchema = z.object({
 	otp: z.string(),
@@ -22,44 +23,21 @@ const app = new Elysia()
 	)
 	.get("/", () => ({ message: "Hello, world!" }))
 	.post("/emails/verify-otp", async ({ body }) => {
-		console.log("body", body);
 		const { email, otp, emailVerifyURL } = VerifyOTPSchema.parse(body);
-		console.log("Sending email to", email, otp);
-		const sesClient = new SESClient({
-			region: "ap-southeast-2", // replace with your region
-			credentials: {
-				accessKeyId: process.env.AWS_EMAIL_ACCESS_KEY ?? "",
-				secretAccessKey: process.env.AWS_EMAIL_SECRET_KEY ?? "",
-			},
-		});
-		const params = {
-			Destination: {
-				ToAddresses: [email],
-			},
-			Message: {
-				Body: {
-					Html: {
-						Charset: "UTF-8",
-						Data: getOtpHTML({
-							otp,
-							verifyURL: emailVerifyURL.toString(),
-						}),
-					},
-				},
-				Subject: {
-					Data: "Verify your email",
-				},
-			},
-			Source: "opachimari@gmail.com",
-		};
-		try {
-			const command = new SendEmailCommand(params);
-			await sesClient.send(command);
+		const resend = new Resend(process.env.RESEND_API_KEY);
 
-			return { status: "success" };
-		} catch (err) {
-			console.error("Error sending email:", err);
-			return error(500, "Error sending email");
+		const data = await resend.emails.send({
+			from: "blazell@blazell.com",
+			to: [email],
+			subject: "Verify Email",
+			html: getOtpHTML({
+				otp,
+				verifyURL: emailVerifyURL.toString(),
+			}),
+		});
+		if (data.error) {
+			console.error(data.error);
+			return error(500, "Failed to send email");
 		}
 	})
 	.listen(8080);
